a19f887a187d2d582ba099b633c33b95fcf6f42b
ahyeona <ahhyeon741@gmail.com>
2023-08-04 11:42:40 +0900

    app.js수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import NavHeader from './components/navbar/NavHeader';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Insert from './components/insertPage/Insert';
+import "./App.css";
+import NavHeader from "./components/navbar/NavHeader";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Insert from "./components/insertPage/Insert";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/insert' element={<Insert/>} />
-      </Routes>
       <NavHeader></NavHeader>
+      <Routes>
+        <Route path="/insert" element={<Insert />} />
+      </Routes>
     </div>
   );
 }
