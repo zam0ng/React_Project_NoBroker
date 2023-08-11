1979991ac33c0ecf549ca4210caf61384e0aeef9
wijiwon <dnlwldnjs5@gmail.com>
2023-08-11 14:22:50 +0900

    main 페이지 작업 중

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,24 @@ const express = require("express");
 const dot = require("dotenv").config();
 
 const app = express();
+// const mine = require('mime-types')
+const { sequelize } = require("./models");
 
-const {sequelize} = require("./models");
-
-app.use(express.urlencoded({extended :false}));
+app.use(express.urlencoded({ extended: false }));
 
 sequelize
-    .sync({ force: true })
-    .then(() => {
-        console.log("database Connect");
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+  .sync({ force: false })
+  .then(() => {
+    console.log("database Connect");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
+const mainRouter = require("./router/main");
 
-const server = app.listen(8080,()=>{
-    console.log("Server on");
-})
+app.use("/", mainRouter);
+
+const server = app.listen(8080, () => {
+  console.log("Server on");
+});
--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <Main></Main> */}
+      <Main />
     </div>
   );
 }
