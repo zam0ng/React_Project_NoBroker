f36e49061568c8442819f57a066e5863c25ad9be
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2023-08-17 14:47:01 +0900

    edit

--- front/src/Axios.js
+++ front/src/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set default base URL for API requests
-axios.defaults.baseURL = 'http://3.37.244.154:8080';    // aws 에서 탄력적 ip 주소 넣음
+axios.defaults.baseURL = 'http://3.37.244.154:8080/api';    // aws 에서 탄력적 ip 주소 넣음
 // axios.defaults.baseURL = 'http://localhost:8080';
 
 // Set default headers, such as authorization token
--- front/src/components/serverURL.js
+++ front/src/components/serverURL.js
@@ -1,2 +1,2 @@
-export const serverUrl = "http://3.37.244.154:8080"         // aws 에서 탄력적 ip 주소 넣음
+export const serverUrl = "http://3.37.244.154:8080/api"         // aws 에서 탄력적 ip 주소 넣음
 
